f25ad956f8ef9e87862cfd933cdffc91f6270869
Batuhannsari <batuhan.sari@aselsis.com.tr>
2023-08-28 09:35:12 +0300

    newAPIkey for requests

--- src/utils/queryBuilder.ts
+++ src/utils/queryBuilder.ts
@@ -1,6 +1,6 @@
 export const queryBuilder = (params: any) => {
 
-    const query = new URLSearchParams({ apikey: "8e2b0a83", ...params }).toString()
+    const query = new URLSearchParams({ apikey: "495a0602", ...params }).toString()
     return query
     
     
